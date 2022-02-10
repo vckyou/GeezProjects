f42c487cc70b538c6beb61e3d6730118fc03177b
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-10 19:51:51 +0700

    Update pmbot.py

--- userbot/modules/pmbot.py
+++ userbot/modules/pmbot.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2021 Catuserbot <https://github.com/sandy1709/catuserbot>
-# Ported by @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
-# t.me/SharingUserbot & t.me/Lunatic0de
+# recode by @vckyaz
+# FROM GeezProjects <https://github.com/vckyou/GeezProjects>
+#
+# Support @GeezSupport & @GeezProjects
 
 import asyncio
 from datetime import datetime
