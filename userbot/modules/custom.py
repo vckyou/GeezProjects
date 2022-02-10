7e189f53b5f16b951be04d8286a33982dad1b6e1
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-10 19:54:12 +0700

    Update custom.py

--- userbot/modules/custom.py
+++ userbot/modules/custom.py
@@ -2,11 +2,10 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
+# recode by @vckyaz
+# FROM GeezProjects <https://github.com/vckyou/GeezProjects>
 #
-# Recode by @mrismanaziz
-# FROM Man-Userbot
-# t.me/SharingUserbot
-#
+# Support @GeezSupport & @GeezProjects
 
 import io
 
