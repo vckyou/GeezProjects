7e834bb80baa51f4ef979f1e74ba5a7cf9b189ed
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-10 19:30:01 +0700

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -3,9 +3,6 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Recode by @mrismanaziz
-# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
-# t.me/SharingUserbot & t.me/Lunatic0de
 
 import logging
 from asyncio import sleep
