9549dbc2f5a3992cb2a300c9ce92142349595564
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-11 02:56:51 +0700

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -3,7 +3,6 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Recode by @mrismanaziz
 # FROM GeezProjects <https://github.com/vckyou/GeezProjects>
 # Support @GeezSupport & @GeezProjects
 
