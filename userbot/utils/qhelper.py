c9250d3d36960819a2ef2497c9db109eca943fc8
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-15 11:41:22 +0700

    Update qhelper.py

--- userbot/utils/qhelper.py
+++ userbot/utils/qhelper.py
@@ -22,7 +22,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import types
 
-from .utils import _geezutils
+from userbot.utils.tools import runcmd as _geezutils
 
 COLORS = [
     "#F07975",
