e03a1f18c526a63cdcd6967124dc391d6fd4f83a
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-15 12:01:05 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -228,35 +228,33 @@ async def kang(args):
         return await edit_delete(
             args, "**Silahkan Reply Ke Pesan Media Untuk Mencuri Sticker Pack itu!**"
         )
-
-    if isinstance(message.media, MessageMediaPhoto):
-        xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
-        photo = io.BytesIO()
-        photo = await args.client.download_media(message.photo, photo)
-    elif "image" in message.media.document.mime_type.split("/"):
-        xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
-        photo = io.BytesIO()
-        await args.client.download_file(message.media.document, photo)
-        if (
-            DocumentAttributeFilename(file_name="sticker.webp")
-            in message.media.document.attributes
-        ):
-            emoji = message.media.document.attributes[1].alt
-            if emoji != "✨":
+    if message and message.media:
+        if isinstance(message.media, MessageMediaPhoto):
+            xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
+            photo = io.BytesIO()
+            photo = await args.client.download_media(message.photo, photo)
+        elif "image" in message.media.document.mime_type.split("/"):
+            xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
+            photo = io.BytesIO()
+            await args.client.download_file(message.media.document, photo)
+            if (
+                DocumentAttributeFilename(file_name="sticker.webp")
+                in message.media.document.attributes
+            ):
+                emoji = message.media.document.attributes[1].alt
+               if emoji != "✨":
                 emojibypass = True
-    elif "tgsticker" in message.media.document.mime_type:
-        xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
-        await args.client.download_file(message.media.document, "AnimatedSticker.tgs")
-
-        attributes = message.media.document.attributes
-        for attribute in attributes:
-            if isinstance(attribute, DocumentAttributeSticker):
-                emoji = attribute.alt
-
-        emojibypass = True
-        is_anim = True
-        photo = 1
-    elif message.media.document.mime_type in ["video/mp4", "video/webm"]:
+        elif "tgsticker" in message.media.document.mime_type:
+            xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
+            await args.client.download_file(message.media.document, "AnimatedSticker.tgs")
+            attributes = message.media.document.attributes
+            for attribute in attributes:
+                if isinstance(attribute, DocumentAttributeSticker):
+                    emoji = attribute.alt
+            emojibypass = True
+            is_anim = True
+            photo = 1
+        elif message.media.document.mime_type in ["video/mp4", "video/webm"]:
             if message.media.document.mime_type == "video/webm":
                 xx = await edit_or_reply(args, f"`{random.choice(KANGING_STR)}`")
                 sticker = await args.client.download_media(
