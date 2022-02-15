053493688ef5355d4af4f3a78ffb2bf956da5ab0
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-16 00:40:27 +0700

    ya

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -12,43 +12,69 @@ from telethon.tl.functions.contacts import UnblockRequest
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, geez_cmd
+from userbot.utils.tools import create_quotly
 
+from .carbon import all_col
 
 @geez_cmd(pattern="q ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    if not event.reply_to_msg_id:
-        return await edit_delete(event, "**Mohon Balas ke Pesan**")
-    reply_message = await event.get_reply_message()
-    warna = event.pattern_match.group(1)
-    chat = "@QuotLyBot"
-    xx = await edit_or_reply(event, "`Processing...`")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1031952739)
-            )
-            first = await conv.send_message(f"/qcolor {warna}")
-            ok = await conv.get_response()
-            await asyncio.sleep(2)
-            second = await event.client.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await event.client(UnblockRequest(chat))
-            first = await conv.send_message(f"/qcolor {warna}")
-            ok = await conv.get_response()
-            await asyncio.sleep(2)
-            second = await event.client.forward_messages(chat, reply_message)
-            response = await response
-        if response.text.startswith("Hi!"):
-            await xx.edit("**Mohon Menonaktifkan Pengaturan Privasi Forward Anda**")
+async def quott_(event):
+    match = event.pattern_match.group(1).strip()
+    if not event.is_reply:
+        return await edit_delete("`Reply Kepesan!`")
+    msg = await await edit_or_reply(event, "Sedang Memprosess!!"))
+    reply = await event.get_reply_message()
+    replied_to, reply_ = None, None
+    if match:
+        spli_ = match.split(maxsplit=1)
+        if (spli_[0] in ["r", "reply"]) or (
+            spli_[0].isdigit() and int(spli_[0]) in range(1, 21)
+        ):
+            if spli_[0].isdigit():
+                if not event.client._bot:
+                    reply_ = await event.client.get_messages(
+                        event.chat_id,
+                        min_id=event.reply_to_msg_id - 1,
+                        reverse=True,
+                        limit=int(spli_[0]),
+                    )
+                else:
+                    id_ = reply.id
+                    reply_ = []
+                    for msg_ in range(id_, id_ + int(spli_[0])):
+                        msh = await event.client.get_messages(event.chat_id, ids=msg_)
+                        if msh:
+                            reply_.append(msh)
+            else:
+                replied_to = await reply.get_reply_message()
+            try:
+                match = spli_[1]
+            except IndexError:
+                match = None
+    user = None
+    if not reply_:
+        reply_ = reply
+    if match:
+        match = match.split(maxsplit=1)
+    if match:
+        if match[0].startswith("@") or match[0].isdigit():
+            try:
+                match_ = await event.client.parse_id(match[0])
+                user = await event.client.get_entity(match_)
+            except ValueError:
+                pass
+            match = match[1] if len(match) == 2 else None
         else:
-            await xx.delete()
-            await event.client.send_file(event.chat_id, response.message)
-    await event.client.delete_messages(
-        conv.chat_id, [first.id, ok.id, second.id, response.id]
-    )
+            match = match[0]
+    if match == "random":
+        match = choice(all_col)
+    try:
+        file = await create_quotly(reply_, bg=match, reply=replied_to, sender=user)
+    except Exception as er:
+        return await msg.edit(str(er))
+    message = await reply.reply("Quotly by GeezProjects", file=file)
+    os.remove(file)
+    await msg.delete()
+    return message
 
 
 CMD_HELP.update(
