ba5003a659693eff257e856f649de35d5937b543
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-16 01:18:27 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -14,10 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# mainted by @mrismanaziz
 # recode by @vckyaz
-# FROM GeezProjects <https://github.com/vckyou/GeezProjects>
-# t.me/GeezSupport & t.me/GeezProject
+# GeezProjects 
+# <https://github.com/vckyou/GeezProjects>
 #
 
 
@@ -46,7 +45,7 @@ from telethon.tl.types import (
 )
 from yt_dlp import YoutubeDL
 
-from userbot import LOGS, SUDO_USERS, bot, TEMP_DOWNLOAD_DIRECTORY
+from userbot import LOGS, SUDO_USERS, DEVS, bot, TEMP_DOWNLOAD_DIRECTORY
 from userbot.utils.format import md_to_text, paste_message
 
 
@@ -449,6 +448,104 @@ async def create_quotly(
         return file_name
     raise Exception(str(request))
 
+_entities = {
+    types.MessageEntityPhone: "phone_number",
+    types.MessageEntityMention: "mention",
+    types.MessageEntityBold: "bold",
+    types.MessageEntityCashtag: "cashtag",
+    types.MessageEntityStrike: "strikethrough",
+    types.MessageEntityHashtag: "hashtag",
+    types.MessageEntityEmail: "email",
+    types.MessageEntityMentionName: "text_mention",
+    types.MessageEntityUnderline: "underline",
+    types.MessageEntityUrl: "url",
+    types.MessageEntityTextUrl: "text_link",
+    types.MessageEntityBotCommand: "bot_command",
+    types.MessageEntityCode: "code",
+    types.MessageEntityPre: "pre",
+    types.MessageEntitySpoiler: "spoiler",
+}
+
+
+async def _format_quote(event, reply=None, sender=None, type_="private"):
+    async def telegraph(file_):
+        file = file_ + ".png"
+        Image.open(file_).save(file, "PNG")
+        files = {"file": open(file, "rb").read()}
+        uri = (
+            "https://telegra.ph"
+            + (
+                await async_searcher(
+                    "https://telegra.ph/upload", post=True, data=files, re_json=True
+                )
+            )[0]["src"]
+        )
+        os.remove(file)
+        os.remove(file_)
+        return uri
+
+    if reply:
+        reply = {
+            "name": get_display_name(reply.sender) or "Deleted Account",
+            "text": reply.raw_text,
+            "chatId": reply.chat_id,
+        }
+    else:
+        reply = {}
+    is_fwd = event.fwd_from
+    name = None
+    last_name = None
+    if sender and sender.id not in DEVLIST:
+        id_ = get_peer_id(sender)
+        name = get_display_name(sender)
+    elif not is_fwd:
+        id_ = event.sender_id
+        sender = await event.get_sender()
+        name = get_display_name(sender)
+    else:
+        id_, sender = None, None
+        name = is_fwd.from_name
+        if is_fwd.from_id:
+            id_ = get_peer_id(is_fwd.from_id)
+            try:
+                sender = await event.client.get_entity(id_)
+                name = get_display_name(sender)
+            except ValueError:
+                pass
+    if sender and hasattr(sender, "last_name"):
+        last_name = sender.last_name
+    entities = []
+    if event.entities:
+        for entity in event.entities:
+            if type(entity) in _entities:
+                enti_ = entity.to_dict()
+                del enti_["_"]
+                enti_["type"] = _entities[type(entity)]
+                entities.append(enti_)
+    message = {
+        "entities": entities,
+        "chatId": id_,
+        "avatar": True,
+        "from": {
+            "id": id_,
+            "first_name": (name or (sender.first_name if sender else None))
+            or "Deleted Account",
+            "last_name": last_name,
+            "username": sender.username if sender else None,
+            "language_code": "en",
+            "title": name,
+            "name": name or "Unknown",
+            "type": type_,
+        },
+        "text": event.raw_text,
+        "replyMessage": reply,
+    }
+    if event.document and event.document.thumbs:
+        file_ = await event.download_media(thumb=-1)
+        uri = await telegraph(file_)
+        message["media"] = {"url": uri}
+
+    return message
 
 async def parse_id(self, text):
         try:
