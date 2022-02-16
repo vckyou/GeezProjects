374a4795268162f22bcbcac5b7087f2cba40bff0
vckyou <viicky.auliaz@gmail.com>
2022-02-16 13:24:43 +0000

    geezprojects: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -17,7 +17,6 @@ from telethon.tl import types
 from telethon.utils import get_display_name
 from youtubesearchpython import VideosSearch
 
-from asyncio import sleep
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot import PLAY_PIC as fotoplay
