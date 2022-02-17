68c2df12a4ccf5bdb84ccc4531ebb34d9c21c682
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-18 03:15:59 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -49,7 +49,7 @@ from telethon.tl.types import (
 from telethon.tl import types
 from yt_dlp import YoutubeDL
 
-from userbot import DEVS, LOGS, SUDO_USERS, TEMP_DOWNLOAD_DIRECTORY, bot
+from userbot import DEVS, LOGS, SUDO_USERS, TEMP_DOWNLOAD_DIRECTORY, owner, bot
 from userbot.utils.format import md_to_text, paste_message
 
 
@@ -616,7 +616,6 @@ def json_parser(data, indent=None):
 
 
 async def metadata(file):
-    who = await file.get_user()
     out, _ = await bash(f'mediainfo """{file}""" --Output=JSON')
     data = {}
     _info = json.loads(out)["media"]["track"]
@@ -630,7 +629,7 @@ async def metadata(file):
     if info.get("AudioCount"):
         data["title"] = info.get("Title", file)
         data["performer"] = (
-            info.get("Performer") or info.get("artist") or str(who.first_name)
+            info.get("Performer") or info.get("artist") or info.get(f"owner")
         )
     if info.get("VideoCount"):
         data["height"] = int(float(_info[1].get("Height", 720)))
