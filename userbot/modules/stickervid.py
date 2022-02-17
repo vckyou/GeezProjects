3f973bb153f990c549fc41e829f8f4a10001989f
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-18 01:01:06 +0700

    Update stickervid.py

--- userbot/modules/stickervid.py
+++ userbot/modules/stickervid.py
@@ -11,7 +11,8 @@ from os import remove
 import cv2
 import requests
 from userbot import LOGS
-from userbot.utils.tools import create_quotly, TgConverter
+from userbot.utils.tools import create_quotly
+from userbot.utils.tgconverter import TgConverter
 from telethon.errors import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeFilename, DocumentAttributeSticker
 from userbot.utils import edit_or_reply, geez_cmd
