8568d307c8cbf0d4372666c51a98fd7dbc9d54a2
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-18 03:05:41 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -616,7 +616,7 @@ def json_parser(data, indent=None):
 
 
 async def metadata(file):
-    bot.me.first_name
+    who = await file.get_user()
     out, _ = await bash(f'mediainfo """{file}""" --Output=JSON')
     data = {}
     _info = json.loads(out)["media"]["track"]
@@ -630,7 +630,7 @@ async def metadata(file):
     if info.get("AudioCount"):
         data["title"] = info.get("Title", file)
         data["performer"] = (
-            info.get("Performer") or info.get("artist") or bot.me.first_name
+            info.get("Performer") or info.get("artist") or str(who.first_name)
         )
     if info.get("VideoCount"):
         data["height"] = int(float(_info[1].get("Height", 720)))
