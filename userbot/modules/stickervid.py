01cb49c24a295b3aa6e8c5d6bed05a151d33a262
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-21 05:55:25 +0700

    Update stickervid.py

--- userbot/modules/stickervid.py
+++ userbot/modules/stickervid.py
@@ -93,6 +93,7 @@ async def kang(args):
                 # pack
                 emoji = splat[1]
 
+        pack += 1
         packname = f"a{user.id}_by_{user.username}_{pack}"
         packnick = f"@{user.username}'s kang pack Vol.{pack}"
         cmd = "/newpack"
