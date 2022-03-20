34c8b7e77bc0cebb55e04772e585ea9d50d0175b
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-20 15:10:39 +0700

    .

---
+++ userbot/clients/logger.py
@@ -0,0 +1,67 @@
+from telethon.tl.functions.channels import InviteToChannelRequest
+
+from userbot import BOT_USERNAME
+from userbot import BOT_VER as version
+from userbot import BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import GEEZ2, GEEZ3, GEEZ4, GEEZ5, bot, branch
+
+MSG_ON = """
+🔥 **GEEZ-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}@{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+━━
+"""
+
+
+async def GEEZ_userbot_on():
+    try:
+        if bot:
+            if BOTLOG_CHATID != 0:
+                await bot.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if GEEZ2:
+            if BOTLOG_CHATID != 0:
+                await GEEZ2.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if GEEZ3:
+            if BOTLOG_CHATID != 0:
+                await GEEZ3.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if GEEZ4:
+            if BOTLOG_CHATID != 0:
+                await GEEZ4.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if GEEZ5:
+            if BOTLOG_CHATID != 0:
+                await GEEZ5.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
+    except BaseException:
+        pass
