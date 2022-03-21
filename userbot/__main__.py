63eacf8f5da510b6e8fa9debef7b24c0556806e5
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-21 16:44:39 +0700

    ..

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -3,34 +3,50 @@
 
 import sys
 from importlib import import_module
-from platform import python_version
 
-from pytgcalls import __version__ as pytgcalls
 from pytgcalls import idle
-from telethon import version
 
 from userbot import BOT_TOKEN
-from userbot import BOT_VER as ubotversion
-from userbot import LOGS, bot
+from userbot import DEVS, LOGS, bot, branch, call_py
 from userbot.clients import geez_userbot_on, multigeez
 from userbot.modules import ALL_MODULES
 from userbot.utils import autobot, checking
 
 try:
-    for module_name in ALL_MODULES:
-        imported_module = import_module(f"userbot.modules.{module_name}")
     client = multigeez()
     total = 5 - client
-    LOGS.info(f"Total Clients = {total} User")
-    LOGS.info(f"Python Version - {python_version()}")
-    LOGS.info(f"Telethon Version - {version.__version__}")
-    LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
-    LOGS.info(f"GeezProjects Version - {ubotversion} < GeezProjects Berhasil Diaktfikan \>")
-except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
-    pass
-except BaseException as e:
+    bot.start()
+    call_py.start()
+    user = bot.get_me()
+    blacklistgeez = requests.get(
+        "https://raw.githubusercontent.com/vckyou/Reforestation/master/blacklistgeez.json"
+    ).json()
+    if user.id in blacklistgeez:
+        LOGS.warning(
+            "NAMPAKNYA USERBOT TIDAK DAPAT BEKERJA, MUNGKIN ANDA TELAH DI BLACKLIST OLEH PEMILIK USERBOT.\nCredits: @VckyouuBitch"        )
+        sys.exit(1)
+    if 1488093812 not in DEVS:
+        LOGS.warning(
+            f"EOL\nGeezProjects v{BOT_VER}, Copyright © 2021-2022 VICKY <https://github.com/vckyou>"
+        )
+        sys.exit(1)
+except Exception as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+
+LOGS.info(
+    f"Total Clients = {total} User"
+)
+
+LOGS.info(
+    f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/GeezSupport"
+)
+
+LOGS.info(f"💢 Geez - Projects Berhasil Diaktfikan 💢",
+            )
 
 
 bot.loop.run_until_complete(checking())
