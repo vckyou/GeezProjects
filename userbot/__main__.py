b8c27dbec2ddd24bb552526feb492bb9077e47fc
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-21 12:32:56 +0700

    .

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,24 +1,24 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Copyright (C) 2021 TeamUltroid for autobot
 """ Userbot start point """
+
 
 import sys
 from importlib import import_module
+from platform import python_version
 
-import requests
+from pytgcalls import __version__ as pytgcalls
 from pytgcalls import idle
-from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon import version
 
-from userbot import BOT_TOKEN, BOT_USERNAME, BOT_VER, BOTLOG_CHATID
-from userbot import DEVS, LOGS, bot, branch, call_py
+from userbot import BOT_TOKEN
+from userbot import BOT_VER as ubotversion
+from userbot import LOGS, bot
+from userbot.clients import geez_userbot_on, multigeez
 from userbot.modules import ALL_MODULES
 from userbot.utils import autobot, checking
 
 try:
+    client = multigeez()
+    total = 5 - client
     bot.start()
     call_py.start()
     user = bot.get_me()
@@ -29,7 +29,7 @@ try:
         LOGS.warning(
             "NAMPAKNYA USERBOT TIDAK DAPAT BEKERJA, MUNGKIN ANDA TELAH DI BLACKLIST OLEH PEMILIK USERBOT.\nCredits: @VckyouuBitch"        )
         sys.exit(1)
-    if 5155140917 not in DEVS:
+    if 1488093812 not in DEVS:
         LOGS.warning(
             f"EOL\nGeezProjects v{BOT_VER}, Copyright © 2021-2022 VICKY <https://github.com/vckyou>"
         )
@@ -42,25 +42,15 @@ for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
 LOGS.info(
+    f"Total Clients = {total} User"
+)
+
+LOGS.info(
     f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/GeezSupport"
 )
 
 LOGS.info(f"💢 Geez - Projects Berhasil Diaktfikan 💢",
             )
-
-async def geez_userbot_on():
-    try:
-        if BOTLOG_CHATID != 0:
-            await bot.send_message(
-                BOTLOG_CHATID,
-                f"💢 Geez - Projects Berhasil Diaktfikan 💢\n╼┅━━━━━╍━━━━━┅╾\n❍▹ `Bot Of :` [{user.first_name}](tg://user?id={user.id})\n❍▹ `BotVer :` {BOT_VER}@{branch}\n╼┅━━━━━╍━━━━━┅╾",
-            )
-    except Exception as e:
-        LOGS.info(str(e))
-    try:
-        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
 
 
 bot.loop.run_until_complete(checking())
