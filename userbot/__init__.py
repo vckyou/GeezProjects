8360fbbfecdbda8f841db779e9137cca2c66f7b6
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-26 05:46:00 +0700

    .

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -123,6 +123,21 @@ while 0 < 6:
 
 del _BLACKLIST
 
+if video_chat.STRING_SESSION:
+    GEEZ_CLIENT = TelegramClient(
+        video_chat.STRING_SESSION,
+        api_id=API_KEY,
+        api_hash=API_HASH)
+    # hmm ...
+    GEEZ_CLIENT.storage.name = video_chat.STRING_SESSION
+else:
+    # https://github.com/pytgcalls/pytgcalls/blob/master/pytgcalls/mtproto/mtproto_client.py#L18
+    userbot.__class__.__module__ = 'telethon.client'
+    GEEZ_CLIENT = userbot
+
+call= PyTgCalls(GEEZ_CLIENT, overload_quiet_mode=True)
+call._env_checker.check_environment()  # pylint: disable=protected-access
+
 SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
 BL_CHAT = {int(x) for x in os.environ.get("BL_CHAT", "").split()}
 
