418e0b0691065dfeb378348db2696f81c4681be8
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-28 00:59:40 +0700

    .

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -11,7 +11,7 @@ from userbot import BOT_TOKEN, BOT_VER, blacklistgeez
 from userbot import DEVS, LOGS, LOOP, bot, call_py, BOTLOG_CHATID
 from userbot.clients import geez_userbot_on, multigeez
 from userbot.modules import ALL_MODULES
-from userbot.utils import autobot, checking
+from userbot.utils import autobot, autocreategroup checking
 
 try:
     client = multigeez()
@@ -46,7 +46,7 @@ LOGS.info(f"💢 Geez - Projects Berhasil Diaktfikan 💢")
 LOOP.run_until_complete(geez_userbot_on())
 LOOP.run_until_complete(checking())
 if not BOTLOG_CHATID:
-    LOOP.run_until_complete(autopilot())
+    LOOP.run_until_complete(autocreategroup())
 if not BOT_TOKEN:
     LOOP.run_until_complete(autobot())
 idle()
