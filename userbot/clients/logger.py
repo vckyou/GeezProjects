70416d1714954c9c42ecbc0239e379e96fef0821
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-28 01:09:47 +0700

    .

--- userbot/clients/logger.py
+++ userbot/clients/logger.py
@@ -7,7 +7,6 @@ from userbot import BOT_VER as version
 from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import GEEZ2, GEEZ3, GEEZ4, GEEZ5, bot, branch
-from userbot.utils import checking
 
 MSG_ON = """
      __⚡ Congratulations ⚡__
@@ -47,8 +46,6 @@ async def geez_userbot_on():
         pass
     try:
         if bot:
-            await checking(bot)
-            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await bot.send_message(
                     BOTLOG_CHATID,
@@ -58,8 +55,6 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ2:
-            await checking(GEEZ2)
-            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ2.send_message(
                     BOTLOG_CHATID,
@@ -69,8 +64,6 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ3:
-            await checking(GEEZ3)
-            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ3.send_message(
                     BOTLOG_CHATID,
@@ -80,8 +73,6 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ4:
-            await checking(GEEZ4)
-            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ4.send_message(
                     BOTLOG_CHATID,
@@ -91,12 +82,14 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ5:
-            await checking(GEEZ5)
-            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ5.send_message(
                     BOTLOG_CHATID,
                     MSG_ON.format(version, branch, cmd),
                 )
+    except BaseException:
+        pass
+    try:
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
     except BaseException:
         pass
