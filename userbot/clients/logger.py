31326a133e6dc17c2ca3feeec52128668efa39ea
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-27 23:29:01 +0700

    .

--- userbot/clients/logger.py
+++ userbot/clients/logger.py
@@ -1,10 +1,13 @@
-from telethon.tl.functions.channels import InviteToChannelRequest
+import asyncio
 
-from userbot import BOT_USERNAME
+from telethon.tl.functions.channels import EditAdminRequest, InviteToChannelRequest
+from telethon.tl.types import ChatAdminRights
+
 from userbot import BOT_VER as version
 from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import GEEZ2, GEEZ3, GEEZ4, GEEZ5, bot, branch
+from userbot.utils import checking
 
 MSG_ON = """
      __⚡ Congratulations ⚡__
@@ -17,8 +20,35 @@ __GeezProhects Has Been Deployed__
 
 
 async def geez_userbot_on():
+    new_rights = ChatAdminRights(
+        add_admins=True,
+        invite_users=True,
+        change_info=True,
+        ban_users=True,
+        delete_messages=True,
+        pin_messages=True,
+        manage_call=True,
+    )
+    try:
+        if bot and tgbot:
+            GEEZAV = await tgbot.get_me()
+            BOT_USERNAME = GEEZAV.username
+            await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
+            await asyncio.sleep(3)
+    except BaseException:
+        pass
+    try:
+        if bot and tgbot:
+            GEEZAV = await tgbot.get_me()
+            BOT_USERNAME = GEEZAV.username
+            await bot(EditAdminRequest(BOTLOG_CHATID, BOT_USERNAME, new_rights, "BOT"))
+            await asyncio.sleep(3)
+    except BaseException:
+        pass
     try:
         if bot:
+            await checking(bot)
+            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await bot.send_message(
                     BOTLOG_CHATID,
@@ -28,6 +58,8 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ2:
+            await checking(GEEZ2)
+            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ2.send_message(
                     BOTLOG_CHATID,
@@ -37,6 +69,8 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ3:
+            await checking(GEEZ3)
+            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ3.send_message(
                     BOTLOG_CHATID,
@@ -46,6 +80,8 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ4:
+            await checking(GEEZ4)
+            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ4.send_message(
                     BOTLOG_CHATID,
@@ -55,14 +91,12 @@ async def geez_userbot_on():
         pass
     try:
         if GEEZ5:
+            await checking(GEEZ5)
+            await asyncio.sleep(3)
             if BOTLOG_CHATID != 0:
                 await GEEZ5.send_message(
                     BOTLOG_CHATID,
                     MSG_ON.format(version, branch, cmd),
                 )
-    except BaseException:
-        pass
-    try:
-        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
     except BaseException:
         pass
