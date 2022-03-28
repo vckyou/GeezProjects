5e154d0ed8c6f9ab89e1891d5f6336241cbd35d3
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-29 06:05:58 +0700

    .

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -483,20 +483,23 @@ async def vc_volume(event):
 async def join_(event):
     geezav = await edit_or_reply(event, f"**Processing**")
     if len(event.text.split()) > 1:
+        chat_id = event.chat_id
         chat = event.pattern_match.group(1)
         try:
             chat = await event.client(GetFullUserRequest(chat))
         except AlreadyJoinedError as e:
+            await call_py.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await asyncio.sleep(3)
             return await edit_delete(event, f"**ERROR:** `{e}`", 30)
         except (NodeJSNotInstalled, TooOldNodeJSVersion):
             return await edit_or_reply(event, "NodeJs is not installed or installed version is too old.")
     else:
-        chat = event.chat_id
         from_user = vcmention(event.sender)
     if not call_py.is_connected:
         await call_py.start()
     await call_py.join_group_call(
-        chat,
+        chat_id,
         AudioPiped(
             'http://duramecho.com/Misc/SilentCd/Silence01s.mp3'
         ),
